3128c6ce63adc411a2ca4bfbd652440490488f1b
Isaias Piña <iax7@users.noreply.github.com>
2025-08-02 01:08:41 -0600

    add destroy and create on turbo

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,6 @@
 class EntriesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_entry, only: %i[show destroy]
 
   def index
     @entries = current_user.entries
@@ -7,7 +8,6 @@ class EntriesController < ApplicationController
   end
 
   def show
-    @entry = current_user.entries.find(params[:id])
   end
 
   def new
@@ -18,14 +18,23 @@ class EntriesController < ApplicationController
     @entry = current_user.entries.new(entry_params)
 
     if @entry.save
-      flash[:notice] = "Entry was successfully created."
+      flash.now[:notice] = "<strong>#{@entry.name}</strong> was successfully created.".html_safe
       respond_to do |format|
         format.html { redirect_to root_path }
         format.turbo_stream { }
       end
     else
-      flash[:alert] = "There was an error creating the entry. Please try again."
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @entry.destroy
+    @default = current_user.entries.first
+    flash.now[:notice] = "<strong>#{@entry.name}</strong> was successfully deleted.".html_safe
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.turbo_stream { }
     end
   end
 
@@ -34,5 +43,9 @@ class EntriesController < ApplicationController
   def entry_params
     # params.require(:entry).permit(:name, :url, :username, :password) # Before Rails 8
     params.expect(entry: [:name, :url, :username, :password])
+  end
+
+  def set_entry
+    @entry = current_user.entries.find(params[:id])
   end
 end
--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,2 +1,5 @@
 module EntriesHelper
+  def empty_entries_message
+    content_tag(:h1, "No entries", class: "h-100 d-flex align-items-center justify-content-center")
+  end
 end
